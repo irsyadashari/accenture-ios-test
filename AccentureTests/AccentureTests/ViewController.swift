b2dd68666cbeafbfb8183b600930f89000c47fc1
Irsyad Ashari <irsyad.ashari17@gmail.com>
2024-06-10 19:41:40 +0700

    add all

---
+++ AccentureTests/AccentureTests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccentureTests
+//
+//  Created by Irsyad Ashari on 02/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
